bddf8aaf8d87aeff24c81a65ec5d716d4cc8edd8
ReggeMeister <mattiaswaern04@gmail.com>
2026-01-07 12:32:05 +0100

    commit

---
+++ training.js
@@ -0,0 +1,53 @@
+const button = document.getElementById('button');
+const filterButton = document.getElementById('filterButton');
+const result = document.getElementById('result')
+let clickCount = 0;
+
+
+const names = [
+    'Mattias', 
+    'Annika', 
+    'Milo', 
+    'Frida', 
+    'Leo'
+]
+
+button.addEventListener('click', () => {
+result.innerHTML = '';
+
+   names.forEach((name, index ) => {
+    result.innerHTML += `${name} är index ${index} <br>`
+   })
+
+});
+
+
+
+const personer = [
+    {name: "Mattias", age: 18},
+    {name: "Thomas", age: 23},
+    {name: "Joakim", age: 19},
+    {name: "Joakim", age: 29},
+    {name: "Joakim", age: 132},
+    {name: "Joakim", age: 26}
+];
+
+
+filterButton.addEventListener('click', () => {
+    filterPersoner();
+})
+
+
+function filterPersoner(){
+    const ageValue = Number(document.getElementById('ageToCheck').value);
+
+    const filtered = personer.filter(person => person.age > ageValue);
+
+    const result = document.getElementById('result');
+    result.innerHTML = '';
+
+    filtered.forEach(person => {
+        result.innerHTML += `${person.name} (${person.age}) <br>`;
+    })
+}
+   
