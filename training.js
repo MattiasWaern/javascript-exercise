1235f293747d49e6e3c0f3738cd834437e632b74
ReggeMeister <mattiaswaern04@gmail.com>
2026-01-09 15:01:20 +0100

    nivå 2 o 3 avkladard (dag 2), formatade också alltnig

--- training.js
+++ training.js
@@ -1,119 +1,126 @@
 //Dag 1
 
-const button = document.getElementById('button');
-const btn = document.getElementById('btn');
-const filterButton = document.getElementById('filterButton');
-const result = document.getElementById('result')
-const greetingButton = document.getElementById('greeting') // .addEventListener('click', greeting);
+const button = document.getElementById("button");
+const btn = document.getElementById("btn");
+const filterButton = document.getElementById("filterButton");
+const result = document.getElementById("result");
+const greetingButton = document.getElementById("greeting"); // .addEventListener('click', greeting);
 let clickCount = 0;
 
 // Nivå 1
 
-btn.addEventListener('click', () => {
-    clickCount++;
+btn.addEventListener("click", () => {
+  clickCount++;
 
-    result.innerHTML = (clickCount);
-})
+  result.innerHTML = clickCount;
+});
 
 // Nivå 2
 
-const names = [
-    'Mattias', 
-    'Annika', 
-    'Milo', 
-    'Frida', 
-    'Leo'
-]
+const names = ["Mattias", "Annika", "Milo", "Frida", "Leo"];
 
-button.addEventListener('click', () => {
-result.innerHTML = '';
+button.addEventListener("click", () => {
+  result.innerHTML = "";
 
-   names.forEach((name, index ) => {
-    result.innerHTML += `${name} är index ${index} <br>`
-   })
-
+  names.forEach((name, index) => {
+    result.innerHTML += `${name} är index ${index} <br>`;
+  });
 });
-
 
 // Nivå 3
 
 const personer = [
-    {name: "Mattias", age: 18},
-    {name: "Thomas", age: 23},
-    {name: "Joakim", age: 19},
-    {name: "Joakim", age: 29},
-    {name: "Joakim", age: 132},
-    {name: "Joakim", age: 26}
+  { name: "Mattias", age: 18 },
+  { name: "Thomas", age: 23 },
+  { name: "Joakim", age: 19 },
+  { name: "Joakim", age: 29 },
+  { name: "Joakim", age: 132 },
+  { name: "Joakim", age: 26 },
 ];
 
-filterButton.addEventListener('click', () => {
-    filterPersoner();
-})
+filterButton.addEventListener("click", () => {
+  filterPersoner();
+});
 
-function filterPersoner(){
-    const ageValue = Number(document.getElementById('ageToCheck').value);
+function filterPersoner() {
+  const ageValue = Number(document.getElementById("ageToCheck").value);
 
-    const filtered = personer.filter(person => person.age > ageValue);
+  const filtered = personer.filter((person) => person.age > ageValue);
 
-    const result = document.getElementById('result');
-    result.innerHTML = '';
+  const result = document.getElementById("result");
+  result.innerHTML = "";
 
-    filtered.forEach(person => {
-        result.innerHTML += `${person.name} (${person.age}) <br>`;
-    })
+  filtered.forEach((person) => {
+    result.innerHTML += `${person.name} (${person.age}) <br>`;
+  });
 }
-   
-//  ----------------------------------- Dag 2 -------------------------------------------
 
+//  ----------------------------------- Dag 2 -------------------------------------------
 
 // Nivå 1
 
-greetingButton.addEventListener('click', greeting);
+greetingButton.addEventListener("click", greeting);
 
-function greeting (){
-    result.innerHTML = '';
-    let human = {name: "Mattias", age: 20}
+function greeting() {
+  result.innerHTML = "";
+  let human = { name: "Mattias", age: 20 };
 
-
-    result.innerHTML = ` Mitt namn är ${human.name} och jag är ${human.age} år `
+  result.innerHTML = ` Mitt namn är ${human.name} och jag är ${human.age} år `;
 }
-
 
 // Nivå 2
 
-async function fetchApi(){
-    const response = await fetch('https://jsonplaceholder.typicode.com/users');
-    const data = await response.json();
+async function fetchApi() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await response.json();
 
-    if(!response.ok){
-        throw new Error('Något gick fel');
-    }
+  if (!response.ok) {
+    throw new Error("Något gick fel");
+  }
 
-    console.log(data);
-    return data;
-
+  console.log(data);
+  return data;
 }
 
-async function showUsers(){
+async function showUsers() {
+  const usersDiv = document.getElementById("users");
 
-    try{
+  try {
     const users = await fetchApi();
-    const usersDiv = document.getElementById('users');
-    usersDiv.innerHTML = '';
+    usersDiv.innerHTML = "";
 
     users.forEach(user => {
-        usersDiv.innerHTML += `Name: ${user.name} - Email: ${user.email} <br>`;
-    })
+      const userDiv = document.createElement("div");
+        userDiv.textContent = `${user.name}`;
+        usersDiv.appendChild(userDiv);
+        userDiv.style.cursor = "pointer";
+        userDiv.style.marginBottom = "10px";
 
-    if(!users.length){
-        users.innerHTML = 'Inga användare hittades';
+        userDiv.addEventListener('click', () => {
+            userDiv.innerHTML = `
+            Namn: ${user.name} <br>
+            Email: ${user.email} <br>
+            Adress: ${user.address.street}, ${user.address.city} <br>
+            Telefon: ${user.phone} <br>
+            Företag: ${user.company.name}
+
+            <br>
+            `;
+        });
+
+        usersDiv.appendChild(userDiv);
+    });
+
+    if(users.length === 0 ){
+        userDiv.innerHTML = "Inga anändare hittades";
     }
-    } catch (error){
-        console.log('Error:', error);
-    }
+
+} catch (error) {
+    usersDiv.innerHTML = "Kunde inte hämta användare"
+}
 
 }
 
-showUsers();
 
+showUsers();
 // Nivå 3
