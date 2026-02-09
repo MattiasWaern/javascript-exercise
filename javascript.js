c1e8decde52bfe7f93786b976a022cf74d10cf68
ReggeMeister <mattiaswaern04@gmail.com>
2026-02-09 15:23:11 +0100

    commit

--- javascript.js
+++ javascript.js
@@ -19,9 +19,33 @@ let celsius = prompt("Skriv temperatur Celsius")
 let fahrenheit = (celsius * 1.8) + 32;
 
 console.log(fahrenheit);
-*/
+
 
 const number1 = parseInt(prompt("Skriv första siffran"));
 const number2 = parseInt(prompt("Skriv andra siffran"));
 
 console.log(number1 + number2);
+*/
+
+let guessCount = document.getElementById("guessCount");
+guessCount.innerHTML = 1;
+
+const number =  Math.floor(Math.random () * 10) +1;
+console.log(number);
+
+function checkWin(){
+
+let guess = parseInt(prompt("Skriv en siffra 1-10"));
+
+    if (guess == number) {
+        console.log("Du gissade rätt")
+    } else {
+        console.log("Du gissade fel")
+        guessCount.innerHTML = Number(guessCount.innerHTML) + 1;
+        checkWin();
+    }
+}
+
+checkWin();
+
+
