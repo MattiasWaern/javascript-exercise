c9fee68cf9e9f7790bf55125346487e579cace6c
ReggeMeister <mattiaswaern04@gmail.com>
2026-02-09 14:51:00 +0100

    commit

---
+++ javascript.js
@@ -0,0 +1,27 @@
+/*let name = prompt("SKRIV DITT NAMN")
+
+console.log("Hej " +  name)
+
+
+
+let number = prompt("Enter a number");
+
+function checkOdd(number){
+    return (number % 2 == 0)
+}
+
+checkOdd(number) ? console.log("Jämnt") : console.log("Udda")
+
+
+
+let celsius = prompt("Skriv temperatur Celsius")
+
+let fahrenheit = (celsius * 1.8) + 32;
+
+console.log(fahrenheit);
+*/
+
+const number1 = parseInt(prompt("Skriv första siffran"));
+const number2 = parseInt(prompt("Skriv andra siffran"));
+
+console.log(number1 + number2);
