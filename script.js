967213efc95d868c44606afa27e6d5338093625d
ReggeMeister <mattiaswaern04@gmail.com>
2026-06-11 13:40:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 console.log("Jag är 21 år gammal, heter Mattias och min favoriträtt är Annika");
 console.log("Annika är också min framtida fru <3");
+
+
+const annika = "min framtida fru";
+
+function annikita(){
+const annika = "min flickvän just nu";
+}
+
+
+console.log(annika);
