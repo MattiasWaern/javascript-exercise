622dcc370cbcebc4ff25ab84bc4e341bad7340df
ReggeMeister <mattiaswaern04@gmail.com>
2026-08-12 13:57:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1035,6 +1035,7 @@ console.log(test, test2);
 const button = document.getElementById("color")
 
 
-button.addEventListener('click'), () => {
+button.addEventListener('click', () => {
     console.log("click")
-}
+});
+
