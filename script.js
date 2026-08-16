cb7fe5aa87453dae1eeccfb48abfdcaf9410202f
ReggeMeister <mattiaswaern04@gmail.com>
2026-08-16 16:22:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1081,3 +1081,26 @@ function validateForm(){
     return true;
 }
 
+
+
+
+function apiResponse(){
+    try{
+
+        const response = await fetch ('');
+
+        if(!response.ok){
+            throw new Error;
+        }
+
+
+        const data = await response.json();
+
+        console.log(data)
+
+    } catch (error){
+        console.log("Error:", error)
+    }
+}
+
+apiResponse();
